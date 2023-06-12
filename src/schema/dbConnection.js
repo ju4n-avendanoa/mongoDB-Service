0d0526d872325e52a54f74ad830bb3dcc8a71741
Juan Fernando Avendaño Alzate <121530391+ju4n-avendanoa@users.noreply.github.com>
2023-06-12 17:29:34 -0500

    Update dbConnection.js

--- src/schema/dbConnection.js
+++ src/schema/dbConnection.js
@@ -10,7 +10,7 @@ dotenv.config({ path: ".env" });
  */
 async function dbConnection() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database is connected`);
   } catch (error) {
     throw new Error(error);
